4fa28dd6900998090298c1b160457ee07369ad8a
lightpillar-et <lightpillar2.0@gmail.com>
2025-08-25 17:21:23 +0200

    Time: 15 ms (42.33%), Space: 18.5 MB (21.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s  = sorted (s)
-        t = sorted (t)
-        return s == t
+    
+        
+        return sorted (t)  == sorted (s)
         
