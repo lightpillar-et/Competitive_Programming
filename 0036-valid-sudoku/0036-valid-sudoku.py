99e718b7cb038db5881cc037dc1e2faaa8dbf8de
lightpillar-et <lightpillar2.0@gmail.com>
2025-08-26 15:02:19 +0200

    Time: 5 ms (48.18%), Space: 17.6 MB (92.4%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,41 +1,28 @@
+from collections import defaultdict
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row = {}
-        column = {}
-        sqaures = {}
+        row = defaultdict(set)
+        col = defaultdict(set)
+        square = defaultdict(set)
 
         for i in range(9):
-            row[i] = set()
             for j in range(9):
                 cur = board[i][j]
                 if cur == '.':
                     continue
+
+                key = (i // 3, j // 3)
+
                 if cur in row[i]:
                     return False
-                row[i].add(cur)
-
-        
-        for i in range (9):
-            column[i] = set ()
-            for j in range (9):
-                cur = board[j][i]
-                if cur == ".":
-                    continue 
-                if cur in column[i]:
-                    return False 
-                column[i].add (cur)
-
-
-        for i in range(9):
-            for j in range(9):
-                cur = board[i][j]
-                if cur == '.':
-                    continue
-                key = (i // 3, j // 3)    
-                if key not in sqaures:
-                    sqaures[key] = set()
-                if cur in sqaures[key]:    
+                if cur in col[j]:
                     return False
-                sqaures[key].add(cur)
+                if cur in square[key]:
+                    return False
+
+                row[i].add(cur)
+                col[j].add(cur)
+                square[key].add(cur)
 
         return True
