4da4e33d154e0edc4c0c69173fbe5e691d953147
lightpillar-et <lightpillar2.0@gmail.com>
2025-08-29 10:56:58 +0200

    Time: 7 ms (81.35%), Space: 23.8 MB (5.09%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = "".join(i.lower() for i in s if i.isalnum())
+        s = [i.lower() for i in s if i.isalnum()]
         left , right = 0 , len(s) -1
         return s == s[::-1]
 
