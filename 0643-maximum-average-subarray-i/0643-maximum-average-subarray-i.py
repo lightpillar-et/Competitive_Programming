7ca4624ae30080c8dd399242b4eeb35a0297dcf3
lightpillar-et <lightpillar2.0@gmail.com>
2025-09-01 13:33:47 +0200

    Time: 476 ms (5%), Space: 27.5 MB (39.77%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -6,9 +6,10 @@ class Solution:
         for right in range (len(nums)) :
             window_sum  += nums [right]
             if right  - left +1 == k :
-                res = max (window_sum / k , res)
-                window_sum  = window_sum - nums[left]
-                left +=1 
+                 print (right , left , left +1 )
+                 res = max (window_sum / k , res)
+                 window_sum  = window_sum - nums[left]
+                 left +=1 
         return res
               
         
