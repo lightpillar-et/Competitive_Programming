4bc19e272200aa9cb35acca106a8f4422dc53584
lightpillar-et <lightpillar2.0@gmail.com>
2025-09-01 10:42:32 +0200

    Time: 333 ms (5.52%), Space: 17.7 MB (81.91%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,12 +4,23 @@ class Solution:
         for i in range (len(s)):
             seen = set()
             cur  = 0
-            for j in range (i , len(s)) :
-                if s[j] in seen :
+            # for j in range (i , len(s)) :
+            #     if s[j] in seen :
+            #         break 
+            #     else:
+            #         seen.add(s[j])
+            #         cur +=1 
+            # cnt = max (cnt , cur)
+            while i < len (s) :
+                if s [i] in seen :
                     break 
                 else:
-                    seen.add(s[j])
-                    cur +=1 
-            cnt = max (cnt , cur)
+                    
+                    seen.add (s[i])
+                    i+=1
+                    cur +=1
+            cnt = max  (cnt , cur)
+        
+            
         return cnt
         
