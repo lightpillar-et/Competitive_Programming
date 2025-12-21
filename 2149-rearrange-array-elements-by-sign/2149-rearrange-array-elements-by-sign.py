e88a4875b11d6c2736ed0a6823db3ce897c277c8
lightpillar-et <lightpillar2.0@gmail.com>
2025-12-21 02:09:07 +0100

    Time: 48 ms (53.04%), Space: 43.1 MB (27.14%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -0,0 +1,5 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        pos = [x for x in nums if x > 0]
+        neg = [x for x in nums if x < 0]
+        return [val for pair in zip(pos, neg) for val in pair]
