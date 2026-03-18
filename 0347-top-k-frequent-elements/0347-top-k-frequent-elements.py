aa665f5bb44ac28b4579648a69eb3032c0262464
lightpillar-et <lightpillar2.0@gmail.com>
2026-03-18 07:05:07 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,11 +1,10 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        mapping  = dict(Counter (nums))
-        mapping = sorted (mapping.keys() , key = lambda x : mapping[x] , reverse = True )
-        print (mapping)
-        return mapping [:k]
-        
-      
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        count = {}
 
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
 
-        
+        sorted_nums = sorted(count, key=count.get, reverse=True)
+
+        return sorted_nums[:k]
