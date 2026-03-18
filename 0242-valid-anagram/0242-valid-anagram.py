2fd9dfcaa91943b111a2cba7901b06b9f9ed3328
lightpillar-et <lightpillar2.0@gmail.com>
2026-03-18 04:25:33 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-    
-        
-        return sorted (t)  == sorted (s)
+
+        count = dict()
+
+        for char in s :
+            count[char] =count.get(char , 0) +1
+        for char in t :
+            if char not in count or count[char] == 0 :
+                return False
+            else:
+                count[char] -=1
+        return True 
         
