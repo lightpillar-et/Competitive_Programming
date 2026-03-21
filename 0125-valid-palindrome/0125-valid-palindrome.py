a4248eebd33952697975c64c3753c88cae4b0c9e
lightpillar-et <lightpillar2.0@gmail.com>
2026-03-21 10:23:40 +0100

    Time: 7 ms (81.39%), Space: 19.5 MB (89.43%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,16 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        left, right = 0, len(s) - 1
-        while left < right:
-            while not s[left].isalnum() and left < right:
-                left += 1
-            while left < right and not s[right].isalnum():
-                right -= 1
-
-            if s[left].lower() != s[right].lower():
-                return False
-
-            left += 1
-            right -= 1
-
+#converting all uppercase letters into lowercase letters andremoving all non-alphanumeric characters,
+        words= ""
+        for char in s :
+            if char.isalnum():
+                words += char.lower()
+        left , right = 0 , len(words) -1
+        
+        while left < right :
+            if words[left] != words[right]:
+                return False 
+            else:
+                left +=1
+                right -=1
         return True
+
+        
+        
