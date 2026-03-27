8b4c496720f4d29fbeb1582f4de821da0a97047c
lightpillar-et <lightpillar2.0@gmail.com>
2026-03-27 10:55:14 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.py
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -1,13 +1,17 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        result = ""
+        #converting to list so we can work with chunks 
+        chars = list(s)
+        # we step by 2*k becuase we are working in subsection
+        for start in range(0, len(chars), 2 * k):
 
-        for i in range(0, len(s), 2 * k):
-            chunk = s[i:i + 2 * k]
+            left = start
+            #our reverse portion is the first k letters and based on the constraints if it go out  or if it less than we adjust it like this 
+            right = min(start + k - 1, len(chars) - 1)
+            #reverse 
+            while left < right:
+                chars[left], chars[right] = chars[right], chars[left]
+                left += 1
+                right -= 1
 
-            first_part = chunk[:k]
-            second_part = chunk[k:]
-
-            result += first_part[::-1] + second_part
-
-        return result
+        return "".join(chars)
