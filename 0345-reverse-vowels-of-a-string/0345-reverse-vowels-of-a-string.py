b51cae2d77c0416f37b6110f474f5093bb500073
lightpillar-et <lightpillar2.0@gmail.com>
2026-04-01 18:30:41 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,24 +1,20 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        s = list(s)
-        v = ("a" , "e" , "i" , "o" , "u" , "A" , "E" , "I", "O" , "U")
-        left , right  = 0 , len(s) -1
-        while left < right :
-            if s[left] in v and s[right] in v :
-                temp = s[left]
-                s[left] = s[right]
-                s[right] = temp
-                left +=1
-                right -=1
-            elif s[left] in v and s[right] not in v:
-                right -=1
-            elif s[right] in v and s[left] not in v :
-                left +=1
+        vowels = ("a", "e", "i", "o", "u", "A", "E", "I", "O", "U")
+        chars = list(s)
+        left, right = 0, len(chars) - 1
+
+        while left < right:
+            if chars[left] in vowels and chars[right] in vowels:
+                chars[left], chars[right] = chars[right], chars[left]
+                left += 1
+                right -= 1
+            elif chars[left] in vowels and chars[right] not in vowels:
+                right -= 1
+            elif chars[left] not in vowels and chars[right] in vowels:
+                left += 1
             else:
                 left += 1
                 right -= 1
-            
-        return "".join(s)
 
-
-        
+        return "".join(chars)
