b7b3ee013d36923810d5cb1d603373477fa9bb94
lightpillar-et <lightpillar2.0@gmail.com>
2026-08-13 23:20:44 +0200

    Time: 2 ms (92.08%), Space: 22.9 MB (60.24%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,26 +1,11 @@
 class Solution:
-    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        
-        # Step 1: Count frequency
-        count = {}
-        for num in nums:
-            count[num] = count.get(num, 0) + 1
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        mapping = dict (Counter(nums))
+        mapping  =  sorted (mapping.items() , 
+                            key = lambda x : x [1],
+                              reverse=True)
+        res = []
+        for i in range (k):
+            res. append (mapping [i][0])
+        return res
 
-        # Step 2: Create buckets
-        bucket = []
-        for i in range(len(nums) + 1):
-            bucket.append([])
-
-        # Step 3: Fill buckets
-        for num in count:
-            frequency = count[num]
-            bucket[frequency].append(num)
-
-        # Step 4: Collect top k elements
-        result = []
-
-        for i in range(len(bucket) - 1, 0, -1):
-            for num in bucket[i]:
-                result.append(num)
-                if len(result) == k:
-                    return result
